abc607743f39910e1c8ffe13c5ffaeff04a8a929
Arthur Meric <149785053+Arthur-Meric@users.noreply.github.com>
2026-01-15 12:10:01 +0100

    [E2] [US05] [ST1] & [US] [US06] [ST1]

---
+++ Epic 2 - L'Annuaire/[E2]_[US05]_[ST1].spec.js
@@ -0,0 +1,65 @@
+import { test, expect } from "@playwright/test";
+import { beforeEach } from "node:test";
+
+const urlSite = "https://bugcorp.vercel.app/";
+const liste_input = [
+  "  ",
+  "Dennis Ritchie",
+  "Alex",
+  "Jean",
+  "  Jean",
+  "1001",
+  "100",
+  " 100",
+  "Dennis 1001",
+];
+
+test.beforeEach(async ({ page }) => {
+  console.log(`Running ${test.info().title}\n`);
+  await page.goto(urlSite);
+  await page.getByTestId("nav-directory").click();
+});
+
+test("[E2] [US05] [CT1] - Barre de recherche", async ({ page }) => {
+  for (let i = 0; i < liste_input.length; i++) {
+    console.log('\nTerme recherché : "' + liste_input[i] + '"');
+    await page.getByTestId("reset-btn").click();
+    await page.getByTestId("search-input").click();
+    await page.getByTestId("search-input").fill(liste_input[i]);
+
+    const messageVide = await page.locator("#table-body #table-empty-message"); // Message qui s'affiche dans le tableau quand il est vide
+
+    // On vérifie si le tableau est vide ou non
+    if (await messageVide.isVisible()) {
+      console.log("Le tableau est vide");
+      expect(await messageVide.isVisible()).toBeTruthy();
+    } else {
+      const lignes = await page.locator("#table-employees tbody tr").all(); // Sélectionner toutes les lignes du tableau (sauf l'en-tête)
+      let texteTrouve = true;
+
+      // Parcourir chaque ligne affichée dans le tableau
+      for (let j = 0; j < lignes.length; j++) {
+        const cellule2 = await lignes[j]
+          .locator("td:nth-child(2)")
+          .textContent(); // Récupérer la cellule de la 2e colonne de cette ligne
+        const cellule3 = await lignes[j]
+          .locator("td:nth-child(3)")
+          .locator('[id^="cell-name-"]')
+          .textContent(); // Récupérer la partie "nom" de la cellule de la 3e colonne de cette ligne
+
+        // Le texte recherché doit faire partie de la ligne...
+        if (
+          cellule2.includes(liste_input[i]) ||
+          cellule3.includes(liste_input[i])
+        ) {
+          console.log("Ligne " + (j + 1) + " :", cellule2, cellule3);
+        } else {
+          texteTrouve = false; // Le texte recherché n'a pas été trouvé dans cette ligne
+        }
+      }
+      expect(texteTrouve).toBeTruthy();
+    }
+  }
+
+  // await page.pause(); // Mets en pause l'exécution quand en mode débug
+});
---
+++ Epic 2 - L'Annuaire/[E2]_[US06]_[ST1].spec.js
@@ -0,0 +1,70 @@
+import { test, expect } from "@playwright/test";
+import { beforeEach } from "node:test";
+
+const urlSite = "https://bugcorp.vercel.app/";
+const liste_roles = [
+  "Coffee Maker",
+  "Prompt Engineer",
+  "Scapegoat",
+  "Intern",
+  "Junior Dev",
+  "Senior Dev",
+  "Manager",
+  "Director",
+  "VP",
+  "CEO",
+  "Galactic Emperor",
+];
+
+test.beforeEach(async ({ page }) => {
+  console.log(`Running ${test.info().title}\n`);
+  await page.goto(urlSite);
+  await page.getByTestId("nav-directory").click();
+});
+
+test('[E2] [US05] [CT1] - Ouverture fenêtre "Légende de la BugCorp" (bouton "Légende & Rôles")', async ({
+  page,
+}) => {
+  await page.getByTestId("legend-btn").click();
+  await expect(page.locator("#legend-title")).toContainText(
+    "Légende de la BugCorp"
+  );
+});
+
+test('[E2] [US05] [CT2] - Liste hiérarchique des rôles dans la fenêtre "Légende de la BugCorp"', async ({
+  page,
+}) => {
+  await page.getByTestId("legend-btn").click();
+  await expect(page.locator("#column-roles h3")).toContainText(
+    "Hiérarchie des Rôles"
+  );
+
+  const RoleItems = await page.locator('[id^="list-item-role-"]').all(); // Sélectionner tous les éléments dont l'id commence par "list-item-role"
+
+  for (let i = 0; i < RoleItems.length; i++) {
+    const TitreRoleItem = await RoleItems[i].locator("h4").textContent(); // Récupérer le titre de l'élément
+
+    console.log("Rôle " + (i + 1) + " : " + TitreRoleItem);
+    expect(TitreRoleItem).toBe(liste_roles[i]); // Vérifier que le titre de l'élément correspond bien au texte attendu
+  }
+});
+
+test('[E2] [US05] [CT3a] - Fermer la fenêtre "Légende de la BugCorp" (via le bouton "Compris")', async ({
+  page,
+}) => {
+  await page.getByTestId("legend-btn").click();
+  await page.getByTestId("modal-confirm-btn").click();
+  await expect(page.locator("#page-title")).toContainText(
+    "L'Annuaire Enterprise"
+  );
+});
+
+test('[E2] [US05] [CT3b] - Fermer la fenêtre "Légende de la BugCorp" (via le bouton en forme de croix)', async ({
+  page,
+}) => {
+  await page.getByTestId("legend-btn").click();
+  await page.getByTestId("close-modal-btn").click();
+  await expect(page.locator("#page-title")).toContainText(
+    "L'Annuaire Enterprise"
+  );
+});
